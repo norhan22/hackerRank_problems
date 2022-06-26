d4e5979136150fd073e916da3405231c3e48529e
norhan <nora.moh.2011@gmail.com>
2022-06-26 04:08:07 +0200

    (WIP) 744. Find Smallest Letter Greater Than Target tests

--- LeetCode/Algo/Binary Search/744. Find Smallest Letter Greater Than Target/fun.js
+++ LeetCode/Algo/Binary Search/744. Find Smallest Letter Greater Than Target/fun.js
@@ -4,8 +4,9 @@
  * @return {character}
  */
 var nextGreatestLetter = function (letters, target) {
-  const cond = (e, i) =>
-      e !== target && e > target && (e < letters[i + 1] || e > letters[i - 1]),
+  letters = [...new Set(letters)]; // remove repeated elements
+
+  const cond = (e, i = 0) => e > target,
     binarySearch = () => {
       var start = 0,
         end = letters.length - 1;
@@ -13,12 +14,20 @@ var nextGreatestLetter = function (letters, target) {
         let mid = Math.floor((start + end) / 2),
           guess = letters[mid];
         if (cond(guess, mid)) return guess;
-        else if (guess > target) end = mid - 1;
+
+        if (guess > target) end = mid - 1;
         else start = mid + 1;
       }
+      return letters[0];
     },
-    linearSearch = () => letters.find(cond) || letters[0];
-  if (letters.length > 6) return binarySearch();
-  else return linearSearch();
+    linearSearch = () => letters.find(cond) || letters[0],
+    simpleSlice = () => letters.slice(0, 7).find(cond);
+
+  if (cond(letters[0])) return letters[0];
+
+  if (letters.length > 6) {
+    if (simpleSlice) return simpleSlice();
+    else return binarySearch();
+  } else return linearSearch();
 };
 module.exports = nextGreatestLetter;
--- LeetCode/Algo/Binary Search/744. Find Smallest Letter Greater Than Target/test.js
+++ LeetCode/Algo/Binary Search/744. Find Smallest Letter Greater Than Target/test.js
@@ -20,4 +20,70 @@ describe.only("744. Find Smallest Letter Greater Than Target", function () {
   testFun(["c", "f", "j"], "d", "f");
   testFun(["c", "f", "j"], "j", "c");
   testFun(["e", "e", "e", "k", "q", "q", "q", "v", "v", "y"], "v", "y");
+  testFun(["e", "e", "e", "e", "e", "e", "n", "n", "n", "n"], "a", "e");
+  testFun(["e", "e", "e", "e", "e", "e", "n", "n", "n", "n"], "n", "e");
+  testFun(["e", "e", "e", "k", "q", "q", "q", "v", "v", "y"], "a", "e");
+  testFun(["e", "e", "e", "k", "q", "q", "q", "v", "v", "y"], "e", "k");
+  testFun(
+    [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ],
+    "a",
+    "b"
+  );
+  testFun(
+    [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ],
+    "f",
+    "g"
+  );
 });
